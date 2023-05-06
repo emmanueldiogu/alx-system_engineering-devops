3de342849ec9c4467a8fb0f5115ce28df5a2238b
Emmanuel Diogu <emmanueldiogu@gmail.com>
2023-05-06 00:53:00 -0400

    simple hbn

--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -7,7 +7,7 @@ if ARGV.length != 1
   end
   
 # Find all occurrences of "school" and "School"
-matches = ARGV[0].scan(/^h.*n$/)
+matches = ARGV[0].scan(/^h..*n$/)
 
 # Join the matched substrings into a single string and print it
-puts matches.join
+puts matches
