ad12009cbd32cb097d3c90cb6d72be8239ebcd4c
Emmanuel Diogu <emmanueldiogu@gmail.com>
2023-05-06 01:01:12 -0400

    simple hbn

---
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+# Get the command line argument
+if ARGV.length != 1
+    puts "Usage: ruby 0-simply_match_school.rb <string>"
+    exit 1
+  end
+  
+# Find all occurrences of "school" and "School"
+matches = ARGV[0].scan(/^[A-Z]+$/)
+
+# Join the matched substrings into a single string and print it
+puts matches
